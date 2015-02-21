9794e0d8d2e0aa0dfbc482ff86fdae162cfe27e4
David Lago <davidlago@gmail.com>
2015-02-21 15:33:18 -0500

    Adding events model

--- mbta-rocks.js
+++ mbta-rocks.js
@@ -1,17 +1,9 @@
+Events = new Mongo.Collection("events");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.helpers({
+    events: function () {
+      return Events.find({});
     }
   });
 }
