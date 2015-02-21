a120232a0af8ed6977f0ec609094c579adeddf79
radhikamalik <radhikamalik@dhcp-18-111-22-170.dyn.mit.edu>
2015-02-21 15:55:03 -0500

    adding new events

--- mbta-rocks.js
+++ mbta-rocks.js
@@ -1,11 +1,42 @@
 Events = new Mongo.Collection("events");
 
+function Event(name, line, location, votes){
+	this.name = name;
+	this.line = line;
+	this.location = location;
+	this.votes = votes;
+}
+
+Event.prototype = {
+  save: function() {
+    Events.insert({name: this.name, line: this.line, location: this.location, votes: this.votes });
+  }
+};
+
+
 if (Meteor.isClient) {
   Template.body.helpers({
     events: function () {
       return Events.find({});
     }
   });
+
+  Template.createEvent.events({
+    'click button': function() {
+      var nameInput = $("#nameInput");
+      var name = nameInput.val();
+
+      var lineInput = $("#lineInput");
+      var line = lineInput.val();
+
+      var locationInput = $("#locationInput");
+      var location = locationInput.val();
+
+      var votes = 0;
+
+      new Event(name, line, location, votes).save();
+    }
+  });  
 }
 
 if (Meteor.isServer) {
