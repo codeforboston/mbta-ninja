9c960eebe28797f1db57dd61fe6961945405ad3c
David Lago <davidlago@gmail.com>
2015-02-21 15:40:12 -0500

    Adding upvote-downvote buttons

--- mbta-rocks.js
+++ mbta-rocks.js
@@ -1,9 +1,21 @@
 Events = new Mongo.Collection("events");
 
 if (Meteor.isClient) {
+  // Get a list of all the events
   Template.body.helpers({
     events: function () {
       return Events.find({});
+    }
+  });
+
+  Template.event.events({
+    // Upvote the current event
+    "click .upvote": function () {
+      Events.update(this._id, {$inc: {votes: 1}});
+    },
+    // Downvote the current event
+    "click .downvote": function () {
+      Events.update(this._id, {$inc: {votes: -1}});
     }
   });
 }
