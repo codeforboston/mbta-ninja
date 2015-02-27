4f37cb688caf97c608408b42e3b9706ad410b010
David Lago <davidlago@gmail.com>
2015-02-27 13:57:09 -0500

    Collapsible lines

--- client/templates/landing.js
+++ client/templates/landing.js
@@ -11,4 +11,21 @@ Template.landing.rendered = function() {
       js.src = '//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=504450969581453&version=v2.0';
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));
+
+  // Collapsible lines
+  $('.collapsible').collapsible({
+    accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+  });
+
+  // Make whole div with line clickable
+  $(".collapsible-body").click(function() {
+    window.location = $(this).find("a").attr("href");
+    return false;
+  });
 };
+
+Template.landing.events({
+  'click .line-item': function(e) {
+    $(this).toggleClass('active');
+  }
+});
