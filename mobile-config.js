8ffcc4d82429ddc2bc70ad1a53d8573e32447e7a
David Lago <davidlago@gmail.com>
2015-05-23 13:55:43 -0400

    Android build

---
+++ mobile-config.js
@@ -0,0 +1,13 @@
+App.info({
+  id: 'org.codeforboston.mbta-ninja',
+  name: 'MBTA Ninja',
+  description: 'Crowdsource MBTA T alerts',
+  version: '1.0.0'
+});
+
+App.icons({
+    'android_ldpi': 'resources/icons/icon-ldpi.png',
+    'android_mdpi': 'resources/icons/icon-mdpi.png',
+    'android_hdpi': 'resources/icons/icon-hdpi.png',
+    'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
