f06a94cb7083a408411c538fc9fb62c842b2e4a3
Benjamin Chrobot <bchrobot@mit.edu>
2015-05-26 19:51:37 -0400

    Added iPhone/iPad icons

--- mobile-config.js
+++ mobile-config.js
@@ -2,12 +2,21 @@ App.info({
   id: 'org.codeforboston.mbta-ninja',
   name: 'MBTA Ninja',
   description: 'Crowdsource MBTA T alerts',
+  author: 'Code for Boston',
+  website: 'http://www.mbta.ninja/',
   version: '1.0.1'
 });
 
 App.icons({
+    // Android
     'android_ldpi': 'resources/icons/icon-ldpi.png',
     'android_mdpi': 'resources/icons/icon-mdpi.png',
     'android_hdpi': 'resources/icons/icon-hdpi.png',
-    'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+    'android_xhdpi': 'resources/icons/icon-xhdpi.png',
+
+    // iOS
+    'iphone': 'public/apple-touch-icon-60x60.png',
+    'iphone_2x': 'public/apple-touch-icon-120x120.png',
+    'ipad': 'public/apple-touch-icon-57x57.png',
+    'ipad_2x': 'public/apple-touch-icon-114x114.png'
 });
